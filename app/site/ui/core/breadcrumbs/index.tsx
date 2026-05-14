e5dbc4cff563cbf4028f1394dd69b89912964c8a
wenzf <wenzelfrick@ymail.com>
2026-05-14 10:03:03 +0200

    new `md_1.art`; breadcrumbs padding

--- app/site/ui/core/breadcrumbs/index.tsx
+++ app/site/ui/core/breadcrumbs/index.tsx
@@ -68,7 +68,7 @@ export const Breadcrumbs = () => {
 
 
   return (
-    <nav className="absolute left-2 top-12 z-[9] overflow-auto max-w-full">
+    <nav className="absolute left-2 top-12 z-[9] p-[3px] overflow-auto max-w-full">
       <ol itemScope itemType="https://schema.org/BreadcrumbList"
         className="flex gap-1 [&_li]:items-center [&_a]:hover:underline"
       >
