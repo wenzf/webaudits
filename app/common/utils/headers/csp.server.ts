d893bfba6dbeaba3e4003d41535216c16bbf03cf
wenzf <wenzelfrick@ymail.com>
2026-05-18 05:58:43 +0200

    stricter Referrer-Policy

--- app/common/utils/headers/csp.server.ts
+++ app/common/utils/headers/csp.server.ts
@@ -1,6 +1,6 @@
 import {
-  getCSP, NONE, STRICT_DYNAMIC, SELF,
-  //  UNSAFE_INLINE 
+    getCSP, NONE, STRICT_DYNAMIC, SELF,
+    //  UNSAFE_INLINE 
 } from 'csp-header';
 import headers_remove from './headers_remove.json';
 
@@ -11,59 +11,60 @@ import headers_remove from './headers_remove.json';
 
 
 export const securityHeaders = (nonce: string): Headers => {
-  const owaspHeaders = new Headers();
+    const owaspHeaders = new Headers();
 
-  owaspHeaders.set('Content-Security-Policy', contentSecurityPolicy(nonce));
-  owaspHeaders.set('Cross-Origin-Embedder-Policy', 'same-origin');
-  owaspHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
-  owaspHeaders.set('Cross-Origin-Resource-Policy', 'same-origin');
-  owaspHeaders.set('Origin-Agent-Cluster', '?1');
-  owaspHeaders.set('Permissions-Policy',
-    'accelerometer=(), camera=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()');
-  owaspHeaders.set('Referrer-Policy', 'origin-when-cross-origin');
-  owaspHeaders.set('X-Content-Type-Options', 'nosniff');
-  owaspHeaders.set('X-DNS-Prefetch-Control', 'off');
-  owaspHeaders.set('X-Download-Options', 'noopen');
-  owaspHeaders.set('X-Frame-Options', 'SAMEORIGIN');
-  owaspHeaders.set('X-Permitted-Cross-Domain-Policies', 'none');
-  owaspHeaders.set('X-XSS-Protection', '0');
-  owaspHeaders.set('Strict-Transport-Security',
-    'max-age=63072000; includeSubDomains; preload');
+    owaspHeaders.set('Content-Security-Policy', contentSecurityPolicy(nonce));
+    owaspHeaders.set('Cross-Origin-Embedder-Policy', 'same-origin');
+    owaspHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
+    owaspHeaders.set('Cross-Origin-Resource-Policy', 'same-origin');
+    owaspHeaders.set('Origin-Agent-Cluster', '?1');
+    owaspHeaders.set('Permissions-Policy',
+        'accelerometer=(), camera=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()');
+  //  owaspHeaders.set('Referrer-Policy', 'origin-when-cross-origin');
+    owaspHeaders.set('X-Content-Type-Options', 'nosniff');
+    owaspHeaders.set('X-DNS-Prefetch-Control', 'off');
+    owaspHeaders.set('X-Download-Options', 'noopen');
+    owaspHeaders.set('X-Frame-Options', 'SAMEORIGIN');
+    owaspHeaders.set('X-Permitted-Cross-Domain-Policies', 'none');
+    owaspHeaders.set('X-XSS-Protection', '0');
+    owaspHeaders.set('Strict-Transport-Security',
+        'max-age=63072000; includeSubDomains; preload');
+    owaspHeaders.set('Referrer-Policy', 'strict-origin-when-cross-origin')
 
-  return owaspHeaders;
+    return owaspHeaders;
 };
 
 export const addSecurityHeaders = (headers: Headers, nonce: string): Headers => {
-  const owaspHeaders = securityHeaders(nonce);
+    const owaspHeaders = securityHeaders(nonce);
 
-  owaspHeaders.forEach((value, key) => {
-    headers.set(key, value);
-  });
+    owaspHeaders.forEach((value, key) => {
+        headers.set(key, value);
+    });
 
-  return headers;
+    return headers;
 };
 
 export const sanitizeHeaders = (headers: Headers): Headers => {
-  headers_remove.headers.forEach((header) => {
-    headers.delete(header);
-  });
+    headers_remove.headers.forEach((header) => {
+        headers.delete(header);
+    });
 
-  return headers;
+    return headers;
 };
 
 export const contentSecurityPolicy = (nonce: string): string => {
-  return getCSP({
-    directives: {
-      'script-src': [
-        `${STRICT_DYNAMIC} 'nonce-${nonce}'`
-      ],
-      'object-src': [NONE],
-      'base-uri': [NONE],
-      'upgrade-insecure-requests': true,
-      'frame-ancestors': [NONE],
-      'font-src': [SELF],
-      'form-action': [SELF],
-      'manifest-src': [SELF]
-    }
-  });
+    return getCSP({
+        directives: {
+            'script-src': [
+                `${STRICT_DYNAMIC} 'nonce-${nonce}'`
+            ],
+            'object-src': [NONE],
+            'base-uri': [NONE],
+            'upgrade-insecure-requests': true,
+            'frame-ancestors': [NONE],
+            'font-src': [SELF],
+            'form-action': [SELF],
+            'manifest-src': [SELF]
+        }
+    });
 };
