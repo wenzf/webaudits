fb874408c64a27645f98a6f100ebd7f49dfe1845
wenzf <wenzelfrick@ymail.com>
2026-06-16 07:22:52 +0200

    constrain `redirect_to` in actions

--- app/cms/lib/routes/actions_and_loaders/cms_cud_db.tsx
+++ app/cms/lib/routes/actions_and_loaders/cms_cud_db.tsx
@@ -44,6 +44,7 @@ export const action = async ({ request }: Route.ActionArgs) => {
     if (
         typeof requestType === 'string'
         && typeof redirect_to === "string"
+        && redirect_to.startsWith('/')
         && typeof pk === "string"
         && typeof sk === "string"
         && typeof content_type === "string"
